4d9407e4c3a6d525b6694b8e3f17e243aa089d9e
Sergey Nechaev <serg.nechaev@gmail.com>
2018-12-01 23:15:54 +1100

    Initial commit

---
+++ src/main/java/com/libwave/api/model/common/MetaData.java
@@ -0,0 +1,118 @@
+/*
+
+MIT License
+
+Copyright (c) 2018 libWAVE team
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.libwave.api.model.common;
+
+import java.io.Serializable;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+@SuppressWarnings("serial")
+public class MetaData implements Serializable {
+
+	protected String responseUuid;
+
+	protected long responseDate;
+
+	protected String responseIsoDate;
+
+	private String comment = "";
+
+	{
+
+		responseUuid = DigestUtils.md5Hex(Long.toHexString(ThreadLocalRandom.current().nextLong())).toUpperCase();
+
+		ZonedDateTime utc = ZonedDateTime.now(ZoneOffset.UTC);
+
+		responseDate = utc.toEpochSecond();
+
+		responseIsoDate = DateTimeFormatter.ISO_DATE_TIME.format(utc);
+
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getUuid() {
+		return responseUuid;
+	}
+
+	public long getDate() {
+		return responseDate;
+	}
+
+	public String getIsoDate() {
+		return responseIsoDate;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("MetaData [responseUuid=");
+		builder.append(responseUuid);
+		builder.append(", responseDate=");
+		builder.append(responseDate);
+		builder.append(", responseIsoDate=");
+		builder.append(responseIsoDate);
+		builder.append(", comment=");
+		builder.append(comment);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((responseUuid == null) ? 0 : responseUuid.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MetaData other = (MetaData) obj;
+		if (responseUuid == null) {
+			if (other.responseUuid != null)
+				return false;
+		} else if (!responseUuid.equals(other.responseUuid))
+			return false;
+		return true;
+	}
+
+}
